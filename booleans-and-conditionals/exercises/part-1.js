b47e2b5dad24965be2aa09c2c2654b6e803b9fc4
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-04-30 10:22:16 -0500

    booleans-and-conditionals/exercises/part-1.js

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -4,7 +4,7 @@ let spaceSuitsOn = true;
 let shuttleCabinReady = true;
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
-let shuttleSpeed = 15000;
+let shuttleSpeed = 15000; 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
