4a0924fadd357621d4ce51bf51adb2c917238691
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-05-08 11:51:24 -0500

    arrays1

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,14 @@
 //Create an array called practiceFile with the following entry: 273.15
+let practiceFile = [273.15];
+console.log(typeof practiceFile);
+//Use the bracket notation method to add "42" and "hello" to the array. 
+practiceFile[1] = "hello";
+console.log(practiceFile)
+practiceFile[2] = "42";
+console.log(practiceFile);
+//Add these new items one at a time.  Print the array after each step to confirm the changes.
 
-//Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
-
-//Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+//Use a single .push() to add the following items: 
+//false, -4.6, and "87".  Print the array to confirm the changes.
+practiceFile.push(-4.6, "87");
+console.log(practiceFile);
