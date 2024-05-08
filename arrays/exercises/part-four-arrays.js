e29c6322bd50b9ea796f908fd359e12998105ac7
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-05-08 13:57:18 -0500

    arrays4

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -3,8 +3,16 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
-//1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+//1) Print the result of using concat on the two arrays. Does concat alter the original arrays? 
+//Verify this by printing holdCabinet1 after using the method.
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
 
-//3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+//3) reverse the first array, and sort the second. 
+//What is the difference between these two methods? Do the methods alter the original arrays?
+
+let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+let selectedItems = [];
+
+selectedItems = groceryBag.slice(2, 5).sort();
+console.log(selectedItems);
