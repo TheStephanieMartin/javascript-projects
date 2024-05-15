87236e7670be2e7bc2d20f114904d00b8a03e168
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-05-15 14:19:04 -0500

    working on

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -2,23 +2,56 @@
     a. Print the numbers 0 - 20, one number per line.
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
-    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the 
+    //numbers are multiples of 3. (Your code should work even 
+    //if you replace 50 or 20 with other numbers). */
 
+// //A.
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+// //B
+//  for (let i = 3; i <= 29; i++) {
+//   if (i % 2 !== 0)
+//   console.log(i);
+// }
 
+// //C
+// for (let i = -14; i <= 12; i++) {
+//   if (i % 2 == 0)
+//   console.log(i);
+// }
 
+//D
+// for (let i = 20; i <= 50; i++) {
+//   if (i % 3 == 0)
+//   console.log(i);
+// }
 
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
-
-
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
 
+let myVariable1 = "LaunchCode";
+let myArray1 = [1, 5, "LC101", "blue", 42];
+
+//A
+// for (let i = 0; i < myArray1.length; i++) {
+//   console.log(myArray1[i]);
+// }
+
+//B
+// for (let i = 0; i < myVariable1.length; i++) {
+//   console.log(myVariable1[i]);
+// }
+for (let i = 0; i < myVariable1.length; i++) {
+  console.log(myVariable1[i]);
+}
 
 
-
-
-/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, 
+  -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
