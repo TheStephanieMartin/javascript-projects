9f36cbe8ce0fc9649eeba9f20aa8d99dda7e84ad
TheStephanieMartin <120679623+TheStephanieMartin@users.noreply.github.com>
2024-05-15 14:24:30 -0500

    completed

--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -6,3 +6,12 @@ for (let i = 0; i < str.length; i++) {
 }
 
 console.log(reversed);
+
+let str1 = "pink";
+let reversed1 = "";
+
+for (let i = 0; i < str1.length; i++) {
+    reversed1 = str1[i] + reversed1;
+}
+
+console.log(reversed1);
